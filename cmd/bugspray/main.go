c3d7772da5b37671952c3fa6acade896ec686813
Matthew McPherrin <git@mcpherrin.ca>
2025-02-09 12:35:17 -0500

    insecureSkipVerification flag for proxy
    Useful for some test ACME CAs with bogus certs.

--- cmd/bugspray/main.go
+++ cmd/bugspray/main.go
@@ -18,14 +18,13 @@ func must[T any](v T, err error) T {
 
 func main() {
 	listen := flag.String("listen", ":8080", "Listen Address")
-	target := flag.String("target", "http://localhost:8081", "Target URL")
+	target := flag.String("target", "https://localhost:14000", "Target URL")
+	skipVerify := flag.Bool("insecure-skip-verify", false, "Skip backend certificate verification")
 	flag.Parse()
 
-	acmeProxy := proxy.New(must(url.Parse(*target)))
-	ui := ui.Assets()
-
+	acmeProxy := proxy.New(must(url.Parse(*target)), *skipVerify)
 	mux := http.NewServeMux()
-	mux.Handle("/ui/", http.StripPrefix("/ui/", ui))
+	mux.Handle("/ui/", http.StripPrefix("/ui/", ui.Assets()))
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// Redirect / to the /ui/, and proxy everything else
 		if r.URL.Path == "/" {
--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -2,16 +2,23 @@
 package proxy
 
 import (
+	"crypto/tls"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 )
 
 // New returns a proxy that will proxy ACME requests to the target server.
-func New(target *url.URL) httputil.ReverseProxy {
+func New(target *url.URL, insecureSkipVerify bool) httputil.ReverseProxy {
 	return httputil.ReverseProxy{
 		Rewrite: func(r *httputil.ProxyRequest) {
 			r.SetURL(target)
 			r.Out.Host = r.In.Host
+		},
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: insecureSkipVerify,
+			},
 		},
 	}
 }
